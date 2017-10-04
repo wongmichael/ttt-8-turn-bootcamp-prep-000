9d774d6f2b3e023c2f55815b3b871929f3ca0b6e
Michael Wong <mik9080@gmail.com>
2017-10-04 19:30:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,3 +7,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board,index)
+  index.between?(0,8) && board[index]==" "
+end
+
+def move(board,index,token="X")
+  board[index]=token
+end

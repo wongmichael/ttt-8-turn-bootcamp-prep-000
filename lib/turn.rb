4d33635bf161bfbb7f2e199e914410f8fc91a414
Michael Wong <mik9080@gmail.com>
2017-10-04 19:26:45 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+def display_board(board)
+  row_sep = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts row_sep
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts row_sep
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
